2816beb69ab7fab406f914891801b5bd27f6fe72
Tamara Woodson <twoodson@dn0a219b09.sunet>
2012-12-06 16:04:25 -0800

    Adding modified BlackLitterman and Fit_AR_GARCH files. BlackLitterman now contains a vacuous loop for optimization over an expanding window; Fit_AR_GARCH has been modified to output more diagnostics.

--- BlackLitterman.m
+++ BlackLitterman.m
@@ -43,12 +43,12 @@ regress_err = zeros(m,n);
 for i = 1:n
     LS_coeffs(:,i) = regress(X_rets(:,i), regress_mat);
     single_factor_fit(:,i) = regress_mat*LS_coeffs(:,i);
-    regress_err(:,i) = X_rets(:,i) - single_factor_pred(:,i);
+    regress_err(:,i) = X_rets(:,i) - single_factor_fit(:,i);
 end
 
 
 % Parts (b) and (c)
-[PredictedMean, PredictedCov, Error] = Fit_AR_GARCH(X_rets, m);
+[PredictedMean, PredictedCov, Error, GARCHCoeffs, ARCoeffs, Innos] = Fit_AR_GARCH(X_rets, m);
 
 
 % Part (d): NPEB approach
@@ -84,7 +84,8 @@ weights = [mean(smlo_w); mean(smme_w); mean(smhi_w); mean(bilo_w); mean(bime_w);
 weights = weights/sum(weights);
 
 % asset return for each portfolio
-returns = [ FF(1:window,2) FF(1:window,5) FF(1:window,9) FF(1:window,12) FF(1:window,15) FF(1:window,18)];
+%returns = [ FF(1:window,2) FF(1:window,5) FF(1:window,8) FF(1:window,11) FF(1:window,14) FF(1:window,17)];
+returns = X_rets(1:window,:);
 
 % covariance matrix for returns (taken as known by BL)
 sigma = cov(returns);
@@ -98,33 +99,93 @@ hist_mean = mean(returns, 1);
 
 
 
-% Step (2): Specify views based a "momentum-based factor model" 
+% Step (2): Specify views based on a "momentum-based factor model" 
 
 P = eye(n); % projection matrix
+
+tau = 1/window; % scalar indicating uncertainty of prior
+
+t = window; % current end index
+
+sigma_pi = tau*sigma; % prior covariance matrix
+
 
 q = zeros(n,1); % REPLACE WITH OUTPUT (mean) FROM FORECAST MODEL
 omega = eye(n); % REPLACE WITH OUTPUT (covariance of errors from model)
 
-tau = 1/window; % scalar indicating uncertainty of prior
-
-sigma_pi = tau*sigma; % prior covariance matrix
 
 S = ( sigma_pi\eye(n) + P'*(omega\eye(n))*P )\eye(n); % defines posterior covariance matrix
-m = S*( (sigma_pi\eye(n))*pi + P'*(omega\eye(n))*q ); % defines posterior mean
+M = S*( (sigma_pi\eye(n))*pi + P'*(omega\eye(n))*q ); % defines posterior mean
 
 % Values of mu and sigma used in Black-Litterman portfolio optimization
-mu_BL = m;
+mu_BL = M;
 sigma_BL = S + sigma;
 
+% Replace this with the actual benchmarks from S&P data.
+bench_mu = 0.006;
+bench_t = 0.01;
+
+% NOTE: REPLACE RETURNS ABOVE WITH A MORE GENERALIZED MATRIX AND DEFINE
+% THIS AS THE WINDOW+1ST ROW OF THAT MATRIX.
+%realized_returns = [FF(window+1,2) FF(window+1,5) FF(window+1,8) FF(window+1,11) FF(window+1,14) FF(window+1,17)];
+realized_returns = X_rets(t+1, :);
+
 % now, perform M-V portfolio optimization
-
-
+[w_p, return_p, excess_p, stdev_p] = OptimizePortfolio(mu_BL, sigma_BL, 0, bench_mu, bench_t, realized_returns);
 
 
 
 
 % Step (3): Black-Litterman Master Formula: plot cumulative returns over
 % time
+
+
+% NOTE: incorporate this into step (2) so as to not duplicate code.
+
+excess_returns = zeros(m - t, 1); % keeps track of CUMULATIVE excess returns over time
+total = 0; % keeps track of total excess returns
+iteration = 1; % keeps track of iterations
+
+% while the end of the FF data has not yet been reached, re-applies
+% Black-Litterman and keeps track of cumulative returns.
+while ( t < m )
+     
+    q = zeros(n,1); % REPLACE WITH ACTUAL OUTPUT FROM FORECAST MODEL
+    omega = eye(n); % REPLACE WITH ACTUAL OUTPUT FROM FORECAST MODEL
+    
+    % NOTE: I THINK WE WILL WANT TO RE-COMPUTE PRIOR EQUILIBRIUM WEIGHTS ON
+    % EVERY ITERATION, WITH AN EXPANDING WINDOW.
+    % pi = ...
+
+    S = ( sigma_pi\eye(n) + P'*(omega\eye(n))*P )\eye(n); % defines posterior covariance matrix
+    M = S*( (sigma_pi\eye(n))*pi + P'*(omega\eye(n))*q ); % defines posterior mean
+
+    % Values of mu and sigma used in Black-Litterman portfolio optimization
+    mu_BL = M;
+    sigma_BL = S + sigma;
+
+    
+    % CHECK S&P DATA: maybe use t-1, t instead of t, t+1.
+    bench_mu = mean(SPRets(1:t));
+    bench_t = SPRets(t+1);
+
+    % Realized return
+    realized_returns = X_rets(t+1, :);
+
+    % now, perform M-V portfolio optimization
+    [w_p, return_p, excess_p, stdev_p] = OptimizePortfolio(mu_BL, sigma_BL, 0, bench_mu, bench_t, realized_returns);
+    
+    total = total + excess_p; % updates total
+    excess_returns(iteration) = total; % saves cumulative excess return at time t
+    
+    % updates time and iteration #
+    t = t + 1;
+    iteration = iteration + 1;
+end
+
+% Plots cumulative returns
+figure
+plot(excess_returns);
 
 
 
--- Fit_AR_GARCH.m
+++ Fit_AR_GARCH.m
@@ -9,8 +9,18 @@
 %                        based on a GARCH(1,1) model
 %           Error     -- a vector of differences (r_m - PredMean) between
 %                        the observed and predicted mean @ t = m
+%           CoeffGARCH     -- a matrix of coefficients from GARCH(1,1) fit
+%                            - ith row = ith asset
+%                            - 1st column = conditional mean
+%                            - 2nd column = omega
+%                            - 3rd column = beta
+%                            - 4th column = alpha
+%           CoeffAR        -- a matrix of coefficients from AR(1) fit
+%                            - ith column = ith asset
+%                            - 1st row: intercept
+%                            - 2nd row: phi
 
-function [PredMean, PredCov, Error] = Fit_AR_GARCH(returns, m)
+function [PredMean, PredCov, Error, CoeffGARCH, CoeffAR, Innos] = Fit_AR_GARCH(returns, m)
     
     n = size(returns, 2); % number of assets
     % lagged returns (training)
@@ -24,15 +34,15 @@ function [PredMean, PredCov, Error] = Fit_AR_GARCH(returns, m)
 
     % AR(1) model
     % ---------------------
-    phi = zeros(2,n); % initializes matrix of regression parameters
+    CoeffAR = zeros(2,n); % initializes matrix of regression parameters
     X_fit = zeros(m-2,n); % initializes fit
     X_resid = zeros(m-2,n); % initializes residuals
 
     % regresses each return on its lags to yield phi (AR(1) model)
     for i = 1:n
         reg_mat = [ones(m-2,1) X_lag(:,i)]; % regression matrix (includes y-intercept term)
-        phi(:,i) = reg_mat\X(:,i); % solves for coeffs mu and phi
-        X_fit(:,i) = reg_mat*phi(:,i); % fitted returns
+        CoeffAR(:,i) = reg_mat\X(:,i); % solves for coeffs mu and phi
+        X_fit(:,i) = reg_mat*CoeffAR(:,i); % fitted returns
         X_resid(:,i) = X(:,i) - X_fit(:,i); % residuals
     end
 
@@ -42,7 +52,7 @@ function [PredMean, PredCov, Error] = Fit_AR_GARCH(returns, m)
     % sets specs for GARCH(1,1) model of residuals from AR(1) fit
     spec = garchset('R', 0, 'M', 0, 'P', 1, 'Q', 1, 'display', 'off');
 
-    Coeff = zeros(n,4); % initializes coefficient matrix (one row per asset)
+    CoeffGARCH = zeros(n,4); % initializes coefficient matrix (one row per asset)
     Innos = zeros(m-2,n); % initializes matrix of innovations u
     Sigmas = zeros(m-2,n); % initializes matrix of sigmas
     PredCov = zeros(n,n); % initializes one-step-ahead predicted covariance matrix
@@ -51,7 +61,7 @@ function [PredMean, PredCov, Error] = Fit_AR_GARCH(returns, m)
     % and innovations in their respective pre-initialized matrices
     for i = 1:n
         [co, err, LLF, Innos(:,i), Sigmas(:,i), summary] = garchfit(spec, X_resid(:,i));
-        Coeff(i,:) = [co.C, co.K, co.GARCH, co.ARCH];
+        CoeffGARCH(i,:) = [co.C, co.K, co.GARCH, co.ARCH];
         PredCov(i,i) = co.K + (Sigmas(m-2,i)^2)*co.GARCH + (Innos(m-2, i)^2)*co.ARCH;
     end
     
@@ -69,7 +79,7 @@ function [PredMean, PredCov, Error] = Fit_AR_GARCH(returns, m)
     end
 
     % Predicts mean using  AR(1) model's one-step-ahead forecast
-    PredMean = (phi(1,:))' +  diag(returns(m-1,:))*(phi(2,:)');
+    PredMean = (CoeffAR(1,:))' +  diag(returns(m-1,:))*(CoeffAR(2,:)');
     Error = X_test' - PredMean; % difference between predicted mean and realized returns
 
 end
