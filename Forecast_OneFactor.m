3e9aaa5ed53921dbfbec6618a4799d365a40a3b0
Tamara Woodson <twoodson@Tamaras-MacBook-Air.local>
2012-12-08 05:29:17 -0800

    Added several csv files from Yahoo finance, as well as some simple (as yet untested) functions to apply an AR(1) or other single-factor model to forecasting excess returns.

--- Forecast_OneFactor.m
+++ Forecast_OneFactor.m
@@ -1,31 +1,26 @@
 
 
-
-
-function PredMean = ForecastReturns(r, SP)
+function PredMean = Forecast_OneFactor(r, f)
 
     m = size(r, 1);
     n = size(r, 2); % number of assets
-    
-    % lagged returns
-    X_lag = r(1:m-1, :);
-    SP_lag = SP(1:m-1);
+
 
     % returns
     X = r(2:m, :);
+    f_lag = f(1:m-1);
 
-    %Coeff = zeros(3,n); % initializes matrix of regression parameters
-    Coeff = zeros(n, n+2);
+    Coeff = zeros(n, 2);
     
     X_fit = zeros(m-1,n); % initializes fit
     X_resid = zeros(m-1,n); % initializes residuals
     
+    reg_mat = [ones(m-1,1) f_lag];
+    
     for i = 1:n
-        %reg_mat = [ones(m-1,1) X_lag(:,i) SP_lag]; % regression matrix (includes y-intercept term)
-        reg_mat = [ones(m-1,1) SP_lag X_lag];
         
         %Coeff(:,i) = reg_mat\X(:,i); % solves for coeffs mu and phi
-        Coeff(i,:) = reg_mat\X(:,i);
+        Coeff(i,:) = (reg_mat\X(:,i))';
         
         
         X_fit(:,i) = reg_mat*Coeff(i,:)'; % fitted returns
@@ -33,5 +28,5 @@ function PredMean = ForecastReturns(r, SP)
     end
  
     %PredMean = (Coeff(1,:))' +  diag(r(m,:))*(Coeff(2,:)') + SP(m)*ones(n)*(Coeff(3,:)');
-    PredMean = Coeff* [1; SP(m); r(m,:)'];
+    PredMean = [Coeff(:,1) eye(n)] * [1 r(m,:)'];
 end
